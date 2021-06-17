66e33341e2a6e8b2ec4b2c7d4c389de8ec6c2878
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-17 12:01:56 +0530

    Add files via upload

---
+++ C1 AA2 TeacherCopy.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 600))
+
+paddle1 = pygame.Rect(100, 100, 50, 10)
+paddle2 = pygame.Rect(300, 500, 50, 10)
+ball = pygame.Rect(200, 300, 5, 5)
+
+while True:
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.draw.rect(screen, (100, 100, 100), paddle1)
+    pygame.draw.rect(screen, (100, 100, 100), paddle2)
+    pygame.draw.rect(screen, (0, 0, 0), ball)
+    
+    pygame.display.update()
